d2ce1c2d094bb07fd92a0e3f076be46f7c0d10f9
Rodrigo <relac@minasambiente.com.br>
2020-10-20 10:45:49 -0300

    update script

--- ExtraiCoordenadas.py
+++ ExtraiCoordenadas.py
@@ -1,13 +1,12 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-def load_txt():
-    textoDecreto  = open('DecretoTextoCoordenadas.txt', 'r', encoding='utf-8')
+def loadTextoDecreto(textoDecretoInput):
+    textoDecreto  = open(textoDecretoInput, 'r', encoding='utf-8')
     textoDecreto = textoDecreto.read()
-    textoDecreto = textoDecreto.replace(" ", "")
-    vertices = open('vertices.txt', 'w')
+    textoDecreto = textoDecreto.replace(" ", "").replace("\n", "").upper()
+    vertices = open('vertices' + textoDecretoInput, 'w')
     vertices.write('vertice,x,y' + '\n')
-
-    srch = 'ovérticev'
+    srch = 'OVÉRTICEV'
     start = fcurr = 0
     while start >= 0:
         pos = textoDecreto.find(srch, start)
@@ -18,12 +17,12 @@ def load_txt():
         verticetxt = vertice[0:trimverticeIndex]
         verticetxtLen = len(verticetxt)
         verticeNumber = verticetxt[9:verticetxtLen]
-        trimcoordx = vertice.find('sN')
+        trimcoordx = vertice.find('SN')
         verticecoords = vertice[trimcoordx+2:len(vertice)]
-        verticeYindf = verticecoords.find('me')
+        verticeYindf = verticecoords.find('ME')
         verticeY = verticecoords[0:verticeYindf]
         verticeXp = verticecoords[verticeYindf+3:]
-        verticeXindf1 = verticeXp.find('m')
+        verticeXindf1 = verticeXp.find('M')
         verticeX = verticeXp[0:verticeXindf1]
         linhaVertice = verticeNumber + ',' + verticeX + ',' + verticeY
         vertices.write(linhaVertice + '\n')
@@ -33,4 +32,6 @@ def load_txt():
     vertices.close()
 
 if __name__ == '__main__':
-    load_txt()
+    loadTextoDecreto('TextoDecretoCauaia.txt')
+    loadTextoDecreto('TextoDecretoSerraDaLagoaDourada.txt')
+    loadTextoDecreto('TextoDecretoSerraDeBaldim.txt')
